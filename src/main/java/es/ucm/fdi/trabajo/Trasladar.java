e44576d527e0c501b736e4628470f41d3bef82b2
Inmaculada Pérez <inmape01@ucm.es>
2018-03-09 13:04:04 +0100

    Añadidos métodos sin implementar

---
+++ src/main/java/es/ucm/fdi/trabajo/Trasladar.java
@@ -0,0 +1,24 @@
+package es.ucm.fdi.trabajo;
+
+/**
+ *
+ * @author Inmapg
+ */
+public class Trasladar extends TransformarFuncion{
+    
+    public Trasladar(int newID){
+        super(newID);
+    }
+    
+     public void trasladarX(Politopo p, double d){
+        
+    }
+    
+    public void trasladarY(Politopo p, double d){
+        
+    }
+    
+    public void trasladarZ(Politopo p, double d){
+        
+    }
+}

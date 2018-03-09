0610e23e95d15ab91e289a70796829b2eb44946a
Inmaculada Pérez <inmape01@ucm.es>
2018-03-09 12:58:59 +0100

    Añadidos métodos sin implementar

--- src/main/java/es/ucm/fdi/trabajo/Contraer.java
+++ src/main/java/es/ucm/fdi/trabajo/Contraer.java
@@ -1,5 +1,22 @@
 package es.ucm.fdi.trabajo;
-
+/**
+ *  @author Inmapg
+ */
 public class Contraer extends TransformarFuncion {
-	
+    
+    public Contraer(int newID){
+        super(newID);
+    }
+    
+    public void contraerX(Politopo p, double d){
+        
+    }
+    
+    public void contraerY(Politopo p, double d){
+        
+    }
+    
+    public void contraerZ(Politopo p, double d){
+        
+    }
 }

3b812e0ac9431001b0961d52fe97437ad2590d2c
Inmaculada Pérez <inmape01@ucm.es>
2018-03-09 13:01:37 +0100

    Añadidos métodos sin implementar

---
+++ src/main/java/es/ucm/fdi/trabajo/Girar.java
@@ -0,0 +1,25 @@
+package es.ucm.fdi.trabajo;
+
+/**
+ *
+ * @author Inmapg
+ */
+public class Girar extends TransformarFuncion {
+    
+    public Girar(int newID) {
+        super(newID);
+    }
+    
+    public void girarX(Politopo p, double d){
+        
+    }
+    
+    public void girarY(Politopo p, double d){
+        
+    }
+    
+    public void girarZ(Politopo p, double d){
+        
+    }
+    
+}

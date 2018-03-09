c9c6cf62117eed4b7bc9959682a64c342ddbf0a1
Inmaculada Pérez <inmape01@ucm.es>
2018-03-09 13:16:32 +0100

    Update Politopo.java

--- src/main/java/es/ucm/fdi/trabajo/Politopo.java
+++ src/main/java/es/ucm/fdi/trabajo/Politopo.java
@@ -1,31 +1,29 @@
 package es.ucm.fdi.trabajo;
 
+import java.util.ArrayList;
 /**
  * @author Inmapg
- * @author Eduardo Amaya
- * @author Javier Galiana
- *
  */
 
 public class Politopo {
 	private int dimension;
-	private Vertice[] vertices;
+	private ArrayList<Vertice> vertices; 
 	
         public Politopo(){
             // Not implemented yet
         }
         
-	public Politopo(int dim, Vertice[] v){
+	public Politopo(int dim, ArrayList<Vertice> v){
             dimension = dim;
             vertices = v;
         }
 	/**
 	 * Cambia la lista de vertices
-	 * 
+	 *
 	 * @param v Lista de vertices
 	 * @param dim Dimension
 	 */
-	public void modificar(Vertice[] v, int dim){
+	public void modificar(ArrayList<Vertice> v, int dim){
 		
 	}
 }

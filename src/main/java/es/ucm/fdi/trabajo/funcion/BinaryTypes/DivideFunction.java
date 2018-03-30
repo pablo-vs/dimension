9223a553a5ed6e4b12c8f638d9a1998c39be6a6c
Inmaculada Pérez <inmape01@ucm.es>
2018-03-30 21:16:02 +0200

    Update DivideFunction.java

--- src/main/java/es/ucm/fdi/trabajo/funcion/BinaryTypes/DivideFunction.java
+++ src/main/java/es/ucm/fdi/trabajo/funcion/BinaryTypes/DivideFunction.java
@@ -3,18 +3,23 @@ package main.java.es.ucm.fdi.trabajo.funcion.BinaryTypes;
 import main.java.es.ucm.fdi.trabajo.funcion.FunctionTypes.BinaryFunction;
 import main.java.es.ucm.fdi.trabajo.funcion.FunctionTypes.VariablesList;
 
+/**
+ * @author Javier Navalon
+ * @author Inmapg
+ */
 public class DivideFunction extends BinaryFunction{
 	public DivideFunction(String a, String b) {
-		super(a,b,"Division");
+		super(a, b, "Division");
 	}
+	
+	@Override
 	public String toString() {
-		String ret=null;
-		ret+="(" + f1.toString()+ ")"+"/"+"("+f2.toString() + ")";
-		return ret;
+            StringBuilder ret = new StringBuilder("");
+            ret.append("(").append(f1.toString()).append(") / (").append(f2.toString()).append(")");
+            return ret.toString();
 	}
+	
 	public double evaluate(VariablesList variables) {
-		return (f1.evaluate(variables)/f2.evaluate(variables));
+		return (f1.evaluate(variables) / f2.evaluate(variables));
 	}
 }
-//((x_0^3)*(x_1^(x_1*3))+x_0*3)
-//x^((x^2)+x*(x^2))+x*4

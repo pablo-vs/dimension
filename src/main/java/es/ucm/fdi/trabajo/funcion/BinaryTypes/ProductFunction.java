324ed2b2f350bd3193c6d00b6e7ba0b096541863
Inmaculada Pérez <inmape01@ucm.es>
2018-03-30 21:14:48 +0200

    Update ProductFunction.java

--- src/main/java/es/ucm/fdi/trabajo/funcion/BinaryTypes/ProductFunction.java
+++ src/main/java/es/ucm/fdi/trabajo/funcion/BinaryTypes/ProductFunction.java
@@ -1,19 +1,26 @@
 package main.java.es.ucm.fdi.trabajo.funcion.BinaryTypes;
 
 import main.java.es.ucm.fdi.trabajo.funcion.FunctionTypes.BinaryFunction;
-import main.java.es.ucm.fdi.trabajo.funcion.FunctionTypes.Function;
 import main.java.es.ucm.fdi.trabajo.funcion.FunctionTypes.VariablesList;
 
+/**
+ * @author Javier Navalon
+ * @author Inmapg
+ */
 public class ProductFunction extends BinaryFunction{
 	public ProductFunction(String a, String b) {
-		super(a,b,"Product");
+		super(a, b, "Product");
 	}
+	
+	@Override
 	public String toString() {
-		String ret;
-		ret="(" + f1.toString()+ ")"+"*"+"("+f2.toString() + ")";
-		return ret;
+            StringBuilder ret = new StringBuilder("");
+            ret.append("(").append(f1.toString()).append(") * (").append(f2.toString()).append(")");
+            return ret.toString();
 	}
+        
+	@Override
 	public double evaluate(VariablesList variables) {
-		return (f1.evaluate(variables)*f2.evaluate(variables));
+		return (f1.evaluate(variables) * f2.evaluate(variables));
 	}
 }

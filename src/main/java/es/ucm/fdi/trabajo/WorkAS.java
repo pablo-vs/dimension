e9e668ca53a0f5bdec0e79c4f5d72ec6adcbe3f4
pablo-vs <pavill01@ucm.es>
2018-04-06 11:12:43 +0200

    Created WorkAS

---
+++ src/main/java/es/ucm/fdi/trabajo/WorkAS.java
@@ -0,0 +1,24 @@
+package es.ucm.fdi.trabajo;
+
+import es.ucm.fdi.trabajo.funcion.Function;
+
+public class WorkAS {
+	private ProjectTO project;
+	private ProjectManagerAS projManager = ProjectManagerAS.getInstance();
+
+	public WorkAS(ProjecTO proj) {
+
+	}
+	
+	public void addFunction(Function f) {
+
+	}
+
+	public void addVisualization(Visualization v) {
+
+	}
+
+	public void saveProject() {
+
+	}
+}

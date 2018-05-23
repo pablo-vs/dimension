92019554970655850e8b484dc5ca13262f6e860f
Arturacu <arturoacuaviva@gmail.com>
2018-05-23 16:43:10 +0200

    Update LoginProtocolTest.java

--- src/test/java/es/ucm/fdi/business/network/protocol/LoginProtocolTest.java
+++ src/test/java/es/ucm/fdi/business/network/protocol/LoginProtocolTest.java
@@ -1,15 +1,7 @@
 /*
-  This file is part of Dimension.
-  Dimension is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-  Dimension is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-  You should have received a copy of the GNU General Public License
-  along with Dimension.  If not, see <http://www.gnu.org/licenses/>.
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package es.ucm.fdi.business.network.protocol;
 
@@ -35,11 +27,11 @@ import static org.junit.Assert.*;
  * @author Arturo Acuaviva
  */
 public class LoginProtocolTest {
-    
+
     private LoginProtocol protocolObject;
 
     /**
-     * Creates a login protocol object for a database contanining users. 
+     * Creates a login protocol object for a database contanining users.
      */
     @Before
     public void setUp() {
@@ -47,109 +39,105 @@ public class LoginProtocolTest {
         UserDAO db = new UserDAOHashTableImp();
         // create the protocol to manage the database
         UserManagerAS manager = UserManagerAS.getManager(db);
-        manager.addNewUser(new UserDTO("user1", "James", "eda3eda", Date.from(Instant.now()), 
-        "jamesEDA@is.es", "600323421", "NULL", "Python programmer", UserType.USER, 
-        null));
-        manager.addNewUser(new UserDTO("user2", "Charles", "cr#sd", Date.from(Instant.now()), 
-        "charleston@is.es", "9482039341", "NULL", "Java programmer", UserType.ADMIN, 
-        null));
-        protocolObject = new LoginProtocol(manager); 
-        
+        manager.addNewUser(new UserDTO("user1", "James", "eda3eda", Date.from(Instant.now()),
+                "jamesEDA@is.es", "600323421", "NULL", "Python programmer", UserType.USER,
+                null));
+        manager.addNewUser(new UserDTO("user2", "Charles", "cr#sd", Date.from(Instant.now()),
+                "charleston@is.es", "9482039341", "NULL", "Java programmer", UserType.ADMIN,
+                null));
+        protocolObject = new LoginProtocol(manager);
+
     }
-    
-   
+
     /**
-     * Test of apply method, of class LoginProtocol, using a non-valid package. 
+     * Test of apply method, of class LoginProtocol, using a non-valid package.
      */
     @Test
-    public void testApplyIncorrectPackage(){
+    public void testApplyIncorrectPackage() {
         System.out.println("apply with incorrect package");
         ClientMessage msg = new RequestImage();
-        try{
+        try {
             protocolObject.apply(msg);
             fail("Non valid package used!");
-        }
-        catch(ProtocolException e){
+        } catch (ProtocolException e) {
             // That's fine, the exception was correctly caugth
         }
     }
-    
+
     /**
      * Test of apply method, of class LoginProtocol, using a corrupted package.
-     * The package received is an empty request login package. 
+     * The package received is an empty request login package.
      */
-        @Test
+    @Test
     public void testApplyCorruptedPackage() {
         System.out.println("apply with corrupted package");
         ClientMessage msg = new RequestLogin();
-        try{
+        try {
             protocolObject.apply(msg);
             fail("A corrupted package used!");
-        }
-        catch(ProtocolException e){
+        } catch (ProtocolException e) {
             // That's fine, the exception was correctly caugth
         }
     }
-    
+
     /**
      * Test of apply method, of class LoginProtocol, using a package filled up
-     * with incorrect values. The user in the package does not exist. 
+     * with incorrect values. The user in the package does not exist.
      */
-        @Test
+    @Test
 
-     public void testApplyNonExistingUser() {
+    public void testApplyNonExistingUser() {
         System.out.println("apply with package contanining a non-existing user");
         RequestLogin msg = new RequestLogin();
         msg.username = "Patty";
-        msg.password ="asdf1234";
-        try{
-            if(protocolObject.apply(msg)){
-               fail("LoginProtocol should return false for a non-existing user"); 
+        msg.password = "asdf1234";
+        try {
+            if (protocolObject.apply(msg)) {
+                fail("LoginProtocol should return false for a non-existing user");
             }
-        }catch(ProtocolException e){
+        } catch (ProtocolException e) {
             fail("The package was correctly set up!");
         }
     }
-     
+
     /**
      * Test of apply method, of class LoginProtocol, using a package containing
      * an existing user but with an incorrect password.
      */
-         @Test
+    @Test
 
-     public void testApplyIncorrectPassword() {
+    public void testApplyIncorrectPassword() {
         System.out.println("apply with package with incorrect password");
         RequestLogin msg = new RequestLogin();
         msg.username = "James";
-        msg.password ="asdf1234";
-        try{
-            if(protocolObject.apply(msg)){
-               fail("LoginProtocol should return false for an incorrect password!"); 
+        msg.password = "asdf1234";
+        try {
+            if (protocolObject.apply(msg)) {
+                fail("LoginProtocol should return false for an incorrect password!");
             }
-        }catch(ProtocolException e){
+        } catch (ProtocolException e) {
             fail("The package was correctly set up!");
         }
     }
-     
-      /**
-     * Test of apply method, of class LoginProtocol, using a correct package. 
+
+    /**
+     * Test of apply method, of class LoginProtocol, using a correct package.
      */
-         @Test
+    @Test
     public void testApplyCorrectPackage() throws Exception {
         System.out.println("apply with correct package");
         RequestLogin msg = new RequestLogin();
         msg.username = "James";
         msg.password = "eda3eda";
-        try{
-            if(!protocolObject.apply(msg)){
+        try {
+            if (!protocolObject.apply(msg)) {
                 fail("A correct user and password must return true!");
             }
-        }catch(ProtocolException e){
+        } catch (ProtocolException e) {
             fail("The package was correctly set up!");
         }
     }
-    
-    
+
     /**
      * Test of getSession method, of class LoginProtocol.
      */
@@ -159,15 +147,15 @@ public class LoginProtocolTest {
         RequestLogin msg = new RequestLogin();
         msg.username = "James";
         msg.password = "eda3eda";
-        try{
-            if(!protocolObject.apply(msg)){
+        try {
+            if (!protocolObject.apply(msg)) {
                 fail("A correct user and password must return true!");
             }
-        }catch(ProtocolException e){
+        } catch (ProtocolException e) {
             fail("The package was correctly set up!");
         }
         assertTrue("A valid session must be loaded into the protocol due to James log-in!",
                 protocolObject.getSession() != null);
     }
-    
+
 }
